4b50c0e5d624948cc988602535ec8837041d3a06
Elmot <elijah.mot@gmail.com>
2026-06-22 00:12:57 +0300

    Selector fix, trigger type, gain channel A.

--- Core/Src/oscilloscope.cpp
+++ Core/Src/oscilloscope.cpp
@@ -24,7 +24,7 @@ void dmaMemToMemCallback(DMA_HandleTypeDef* dma_handle_type_def);
 void initialize_test_signal() //todo remove together with tim2 & hdac2 wave generation
 {
     extern const unsigned short fake_signal[];
-    HAL_DAC_Start_DMA(&hdac2, DAC_CHANNEL_1, reinterpret_cast<const uint32_t*>(fake_signal), 140, DAC_ALIGN_12B_R);
+    HAL_DAC_Start_DMA(&hdac2, DAC_CHANNEL_1, reinterpret_cast<const uint32_t*>(fake_signal), 664, DAC_ALIGN_12B_R);
     HAL_TIM_Base_Start(&htim15);
 }
 
@@ -32,24 +32,53 @@ void initialize_test_signal() //todo remove together with tim2 & hdac2 wave gene
  *
  */
 
-constexpr Command CommandBiasChannelA{
-            .name = "vbias.a",
-    .value = -503'000LL, //todo 0
-            .useNewValue = [](long long value)
-            {
-                const uint16_t dac_bias = clamp((value + 1'000'000LL) * 4'095LL / 2'000'000LL, 0LL, 4095LL);
-                HAL_DAC_SetValue(&hdac1, DAC1_CHANNEL_1,DAC_ALIGN_12B_R, dac_bias);
-            }
+constexpr Command CommandGainChannelA{
+    .name = "gain.a",
+    .value = 16,
+
+    .useNewValue = [](const long long value)
+    {
+        uint32_t dac_gain_bits;
+        switch (value)
+        {
+        case 2: dac_gain_bits = OPAMP_PGA_GAIN_2_OR_MINUS_1;
+            break;
+        case 4: dac_gain_bits = OPAMP_PGA_GAIN_4_OR_MINUS_3;
+            break;
+        case 8: dac_gain_bits = OPAMP_PGA_GAIN_8_OR_MINUS_7;
+            break;
+        case 16: dac_gain_bits = OPAMP_PGA_GAIN_16_OR_MINUS_15;
+            break;
+        case 32: dac_gain_bits = OPAMP_PGA_GAIN_32_OR_MINUS_31;
+            break;
+        default: dac_gain_bits = OPAMP_PGA_GAIN_64_OR_MINUS_63;
+        }
+        hopamp3.Init.PgaGain = dac_gain_bits;
+        HAL_OPAMP_Stop(&hopamp3);
+        HAL_OPAMP_Init(&hopamp3);
+        HAL_OPAMP_Start(&hopamp3);
+    },
+    .adjustValue = [](const long long value)
+    {
+        for (const long long i : {2, 4, 8, 16, 32})
+        {
+            if (value <= i) return i;
+        }
+        return 64LL;
+    },
 };
 
-constexpr Command CommandTriggerLevel{
-            .name = "trg_level",
-    .value = 1200'000L, //todo 0
+constexpr Command CommandBiasChannelA{
+    .name = "vbias.a",
+    .value = -503'000LL, //todo 0
+
     .useNewValue = [](const long long value)
-            {
-                const uint16_t dac_bias = clamp((value + 1'000'000LL) * 4'095LL / 2'000'000LL, 0LL, 4095LL);
-                HAL_DAC_SetValue(&hdac1, DAC1_CHANNEL_2,DAC_ALIGN_12B_R, dac_bias);
-            }
+    {
+        const uint16_t dac_bias = clamp(
+            (CommandBiasChannelA.value + 1'000'000LL) * 4'095LL / 2'000'000LL, 0LL, 4095LL);
+        HAL_DAC_SetValue(&hdac1, DAC1_CHANNEL_1,DAC_ALIGN_12B_R, dac_bias);
+    },
+    .adjustValue = [](const long long value) { return clamp(value, -1'000'000LL, 1'000'000LL); },
 };
 
 namespace sampling
@@ -61,7 +90,7 @@ namespace sampling
         .name = "sampling.ns",
         .requiresRestart = true,
         .value = 250,
-        .adjustValue = [](const long long value) { return clamp(value, minSamplingTimeNs, maxSamplingTimeNs); }
+        .adjustValue = [](const long long value) { return clamp(value, minSamplingTimeNs, maxSamplingTimeNs); },
     };
 
     bool isInterleaveSampling()
@@ -77,14 +106,74 @@ namespace sampling
         fullDivider *= HAL_RCC_GetHCLKFreq() * static_cast<unsigned long long>(CommandTimeResolution.value) /
             1'000'000'000ULL;
         return static_cast<uint32_t>(fullDivider - 1);
+    }
+}
+
+
+static void startSampling();
+
+namespace trigger
+{
+    constexpr long long min = -1'000'000;
+    constexpr long long max = 1'000'000;
+    constexpr Command CommandTriggerLevel{
+        .name = "trg.level",
+        .value = 200'000L, //todo 0
+        .useNewValue = [](const long long value)
+        {
+            const uint16_t dac_bias = clamp((value + 1'000'000LL) * 4'095LL / 2'000'000LL, 0LL, 4095LL);
+            HAL_DAC_SetValue(&hdac1, DAC1_CHANNEL_2,DAC_ALIGN_12B_R, dac_bias);
+        },
+        .adjustValue = [](const long long value) { return clamp(value, min, max); }
+    };
+
+
+    constexpr Command CommandTriggerType{
+        .name = "trg.type",
+        .value = 0,
+        .useNewValue = [](const long long value) { startSampling(); },
+        .adjustValue = [](const long long value) { return clamp(value, -1LL, 1LL); },
+
+    };
+
+    void enableTrigger()
+    {
+        if (CommandTriggerType.value == 0)
+        {
+            HAL_NVIC_DisableIRQ(COMP4_5_6_IRQn);
+        }
+        else
+        {
+            HAL_NVIC_EnableIRQ(COMP4_5_6_IRQn);
         }
     }
 
-constexpr array<const Command*, 3> commands{
+    void setupTriggerDelay()
+    {
+        if (sampling::isInterleaveSampling())
+        {
+            __HAL_TIM_SET_AUTORELOAD(&htim1, data_frame_size/2 - 1);
+        }
+        else
+        {
+            __HAL_TIM_SET_AUTORELOAD(&htim1, data_frame_size - 1);
+        }
+        __HAL_TIM_SET_COUNTER(&htim1, 0);
+    }
+
+    uint32_t comparatorValue()
+    {
+        return CommandTriggerType.value == -1 ? COMP_OUTPUT_LEVEL_LOW : COMP_OUTPUT_LEVEL_HIGH;
+    }
+}
+
+constexpr array<const Command*, 5> commands{
     {
         &CommandBiasChannelA,
-        &CommandTriggerLevel,
-        &sampling::CommandTimeResolution
+        &sampling::CommandTimeResolution,
+        &trigger::CommandTriggerLevel,
+        &trigger::CommandTriggerType,
+        &CommandGainChannelA,
     }
 };
 
@@ -101,23 +190,32 @@ static volatile bool triggerArmed = false;
 
 static void startSampling()
 {
+    if (sampling::isInterleaveSampling())
+    {
+        __HAL_TIM_SET_AUTORELOAD(&htim1, data_frame_size/2 - 1);
+    }
+    else
+    {
+        __HAL_TIM_SET_AUTORELOAD(&htim1, data_frame_size - 1);
+    }
+    __HAL_TIM_SET_COUNTER(&htim1, 0);
     startMainAdc(sampling::isInterleaveSampling(), adcBuffer.data(), adcBuffer.size());
     __HAL_TIM_SET_PRESCALER(&htim2, 0);
     __HAL_TIM_SET_AUTORELOAD(&htim2, sampling::clockDivider());
     __HAL_TIM_SET_COUNTER(&htim2, 0);
-    //todo set trigger
     HAL_TIM_Base_Start(&htim2);
     HAL_NVIC_ClearPendingIRQ(COMP4_5_6_IRQn);
     triggerArmed = false;
-
+    trigger::enableTrigger();
 }
+
 static void executeIncomingCommand()
 {
     static char cmdBuffer[121];
     cmdBuffer[0] = 0;
     for (size_t i = 0; true; i = (i + 1) % sizeof(cmdBuffer))
     {
-        osMessageQueueGet(cmdRxQueueHandle, &cmdBuffer[i], 0,osWaitForever);
+        osMessageQueueGet(cmdRxQueueHandle, &cmdBuffer[i], nullptr,osWaitForever);
         if (cmdBuffer[i] == '\n' || cmdBuffer[i] == '\r')
         {
             cmdBuffer[i] = 0;
@@ -134,18 +232,18 @@ static void executeIncomingCommand()
         ptr += command->name.size();
         skipWhiteSpace(ptr);
         if (*ptr++ != '=') continue;
-        long long newValue = atoll(ptr);
+        long long newValue = atoll(ptr); // NOLINT(*-err34-c)
         newValue = command->adjustValue(newValue);
         if (newValue == command->value) continue;
-        command->useNewValue(newValue);
         command->value = newValue;
+        command->useNewValue(newValue);
         requiresRestart |= command->requiresRestart;
         break;
     }
     if (requiresRestart)
     {
         startSampling();
-}
+    }
 }
 
 [[noreturn]] void run_oscilloscope()
@@ -193,15 +291,14 @@ void transmitBufferReady()
     osThreadFlagsSet(transmitTaskHandle, THREAD_FLAG_READY_TO_TRANSMIT);
 }
 
-void dmaMemToMemCallback(DMA_HandleTypeDef* dma_handle_type_def)
+void dmaMemToMemCallback(__unused DMA_HandleTypeDef* dma_handle_type_def)
 {
-    UNUSED(dma_handle_type_def);
     transmitBufferReady();
 }
 
 void HAL_COMP_TriggerCallback(COMP_HandleTypeDef* hcomp)
 {
-    if (HAL_COMP_GetOutputLevel(hcomp))
+    if (HAL_COMP_GetOutputLevel(hcomp) == trigger::comparatorValue())
     {
         if (triggerArmed)
         {
@@ -216,9 +313,8 @@ void HAL_COMP_TriggerCallback(COMP_HandleTypeDef* hcomp)
     HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 }
 
-extern "C" void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef* htim)
+extern "C" void HAL_TIM_PWM_PulseFinishedCallback(__unused TIM_HandleTypeDef* htim)
 {
-    UNUSED(htim);
     HAL_TIM_Base_Stop_IT(&htim1);
     HAL_TIM_Base_Stop(&htim2);
     extern osThreadId_t keyFrameTaskHandle;
@@ -251,6 +347,6 @@ extern "C" [[noreturn]] void keyFramesProcessing()
         osThreadFlagsClear(THREAD_FLAG_KEY_FRAME_DETECTED);
         transmitBufferReady();
         startSampling();
-        HAL_NVIC_EnableIRQ(COMP4_5_6_IRQn);
+        trigger::enableTrigger();
     }
 }
