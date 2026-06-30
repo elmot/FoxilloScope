ec3a1566df0ba79d62d6eb2361df511d9c30f370
Elmot <elijah.mot@gmail.com>
2026-06-30 12:42:46 +0300

    Fix dual mode which was broken because of Vref measurement as injected ADC1 sample

--- Core/Src/adc.c
+++ Core/Src/adc.c
@@ -75,7 +75,7 @@ void MX_ADC1_Init(void)
 
   /** Configure the ADC multi-mode
   */
-  multimode.Mode = ADC_DUALMODE_INTERL;
+  multimode.Mode = ADC_DUALMODE_REGINTERL_INJECSIMULT;
   multimode.DMAAccessMode = ADC_DMAACCESSMODE_DISABLED;
   multimode.TwoSamplingDelay = ADC_TWOSAMPLINGDELAY_6CYCLES;
   if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
